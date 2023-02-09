8ed720ccbfbe658047c26c75004a26b176ed17e8
PrashanthiAdmin <47585730+PrashanthiAdmin@users.noreply.github.com>
2023-02-09 22:37:28 +0530

    Update Runner.java

--- src/test/java/mindq/cucumberframeworkDec/Runner.java
+++ src/test/java/mindq/cucumberframeworkDec/Runner.java
@@ -12,7 +12,7 @@ import io.cucumber.testng.CucumberOptions;
                  glue =     {"mindq.stepdefinitions"},
                 // plugin = { "pretty",//"html:target/cucumber-reports/report.html"},//"json:target/cucumber-reports/Cucumber.json"},
                 		 //"com.cucumber.listener.ExtentCucumberFormatter:target/cucumber-reports/extentreport.html"},
-                		plugin = { "pretty","com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
+                		plugin = { "pretty","json:target/cucumber-reports/Cucumber.json","com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
                  tags =     "@loginwithvaliddata",
                  monochrome = true)
 public class Runner extends AbstractTestNGCucumberTests{
